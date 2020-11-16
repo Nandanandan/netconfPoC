d6d65ce37d4cb33bff134b4da323e64bf0834a02
Nandanandan Mishra <nm00436182@techmahindra.com>
2020-11-16 18:37:54 +0000

    updated few stuffs

--- jnpr_ncclient.py
+++ jnpr_ncclient.py
@@ -6,10 +6,10 @@ if __name__ == "__main__":
     inventory_file = 'inventory.yaml'
 
     with open(inventory_file) as data:
-        inv = yaml.load(data)
+        inv = yaml.safe_load(data)
     # Load the inventory file
         for node in inv['device']:
             obj = ncf(node["ip"], node["username"],node["password"],node["po_ncf"])
+            print(f"Data for {node['lo0']} ...")
             obj.get_interface()
             obj.get_version()
-            print(f"{node['lo0']} in progress...")
--- jnprrpc.py
+++ jnprrpc.py
@@ -12,29 +12,34 @@ class ncfrpc:
         self.passwd = password
         self.nport = po_ncf
 
+    def connectdev(self):
+        jp = nc.connect(host=self.host, username=self.uname, password=self.passwd, port=self.nport, hostkey_verify=False, device_params={'name':'junos'})
+        return jp
+
 
     def get_interface(self):
         filter = '<get-interface-information><terse/></get-interface-information>'
-        try:
-            with nc.connect(host=self.host, username=self.uname, password=self.passwd, port=self.nport, hostkey_verify=False, device_params={'name':'junos'}) as jp:
-                data = jp.rpc(filter)
-                interfaces = data.xpath('//physical-interface/name')
-                interfaces_status = data.xpath('//physical-interface/oper-status')
-                for interface, interface_status in zip(interfaces,interfaces_status):
-                    intf = interface.text.split('\n')[1]
-                    intf_status = interface_status.text.split('\n')[1]
-                    print(f"Interface: {intf:10}, Status: {intf_status.upper():10}")
+        try:            # with nc.connect(host=self.host, username=self.uname, password=self.passwd, port=self.nport, hostkey_verify=False, device_params={'name':'junos'}) as jp:
+            jp = self.connectdev()
+            data = jp.rpc(filter)
+            interfaces = data.xpath('//physical-interface/name')
+            interfaces_status = data.xpath('//physical-interface/oper-status')
+            for interface, interface_status in zip(interfaces,interfaces_status):
+                intf = interface.text.split('\n')[1]
+                intf_status = interface_status.text.split('\n')[1]
+                print(f"Interface: {intf:10}, Status: {intf_status.upper():10}")
         except Exception as e:
             print(f"Exception occured as {e}")
 
     def get_version(self):
         filter = '<get-software-information></get-software-information>'
         try:
-            with nc.connect(host=self.host, username=self.uname, password=self.passwd, port=self.nport, hostkey_verify=False, device_params={'name':'junos'}) as jp:
-                data = jp.rpc(filter)
-                sv = data.xpath('software-information/junos-version')
-                netElm = data.xpath('software-information/host-name')
-                for data1, data2 in zip(sv, netElm):
-                    print(f"\n\nDevice: {data2.text}, Software Version: {data1.text}\n\n")
+            jp = self.connectdev()
+            data = jp.rpc(filter)
+            sv = data.xpath('software-information/junos-version')
+            netElm = data.xpath('software-information/host-name')
+            for data1, data2 in zip(sv, netElm):
+                print(f"\n\nDevice: {data2.text}, Software Version: {data1.text}\n\n")
+
         except Exception as e:
             print(f"Exception occured as {e}")
